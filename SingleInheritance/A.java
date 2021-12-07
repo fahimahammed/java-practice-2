e5c36f432132f760da3beed3468219d5e6083670
NISHI81 <ttanzin081@gmail.com>
2021-12-07 12:17:36 +0600

    update a new file

--- SingleInheritance/A.java
+++ SingleInheritance/A.java
@@ -3,6 +3,6 @@ package SingleInheritance;
 // hi fahim
 public class A {
     public void display1(){
-        System.out.println("Method of class A.");
+        System.out.println("Method of class AB.");
     }
 }
